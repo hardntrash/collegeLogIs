5f8779c509050a4369c3ee4576f04a7ef3a7c6a8
Andrey <hardntrash@gmail.com>
2019-01-21 13:01:37 +0500

    Интерфейс для контроллера

--- LogApp/views.py
+++ LogApp/views.py
@@ -2,7 +2,7 @@ from datetime import date
 from werkzeug.urls import url_parse
 
 from .app import app, session
-from .models import User, Report
+from .models import User, Report, Group
 from flask_login import current_user, login_user, login_required
 from flask import redirect, url_for, flash, render_template, request, jsonify
 from .forms import LoginForm
@@ -45,5 +45,37 @@ def reports_view():
         if request.args.get('date') is not None:
             reports = [report for report in reports if str(report.date) == request.args.get('date')]
             if 'CONTENT_TYPE' in request.headers.environ:
-                return jsonify(render_template('reports.html', reports=reports, teacher=teacher, controller=controller, date_list=date_list))
-        return render_template('reports.html', reports=reports, teacher=teacher, controller=controller, date_list=date_list)
+                return jsonify(render_template('reports.html', reports=reports, user=current_user, controller=controller,
+                                               date_list=date_list))
+        return render_template('reports.html', reports=reports, teacher=teacher, controller=controller,
+                               date_list=date_list)
+    if current_user.id_permission_gorup == 1:
+        reports = session.query(Report).filter(Report.id_controller == current_user.id).all()
+        date_list = set([report.date for report in reports])
+        teachers_list = []
+        groups_list = []
+        for report in reports:
+            teacher = session.query(User).get(report.id_teacher)
+            if teachers_list.count(teacher) < 1:
+                teachers_list.append(teacher)
+        for report in reports:
+            group = session.query(Group).get(report.id_group)
+            if groups_list.count(group) < 1:
+                groups_list.append(group)
+        # print(' '.join(group.name for group in groups_list))
+        # print(' '.join(teacher.first_name for teacher in teachers_list))
+        return render_template('reports.html', user=current_user, reports=reports, date_list=date_list,
+                               teachers_list=teachers_list, groups_list=groups_list)
+
+@app.route('/create_report', methods=['GET', 'POST'])
+@login_required
+def create_report_view():
+    if request.method == 'POST':
+        report = Report(date=date.today(), id_group=int(request.form['group']), id_teacher=int(request.form['teacher']),
+                        comment=request.form['comment'])
+        session.add(report)
+        session.commit()
+        return 'success'
+    groups = session.query(Group).all()
+    teachers = session.query(User).filter(User.id_permission_gorup==2).all()
+    return render_template('create_report.html', date=date.today(), groups=groups, teachers=teachers)
